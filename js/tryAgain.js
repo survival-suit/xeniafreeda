91aa7b85af03a10c2bfaabff56ee6f2924aeb335
survivalsuit <messier83wait@gmail.com>
2021-01-15 16:44:28 +0300

    Поиграл с ф-ей tryAgain.js Настроил плавное удаление.

--- js/tryAgain.js
+++ js/tryAgain.js
@@ -1,19 +1,37 @@
 function tryAgain()
 	{
-		clearInterval(divStringInterval);//остановка интервала мерцания div-текста
+		
 		//прячем div с текстом
-		slowHide(document.getElementById("string"),"1.8s");
-		//прячем кнопку скопировать
-		slowHide(document.getElementById("copybtn"),"1.8s");
-		//прячем кнопку попробовать снова
-		slowHide(document.getElementById("refreshbtn"),"1.8s");
-
+		slowHide(document.getElementById("string"),"2s");
+		setTimeout(
+				function()
+					{
+					//прячем кнопку скопировать
+					slowHide(document.getElementById("copybtn"),"1.4s");
+				}, 500);
+		setTimeout(
+				function()
+					{
+					//прячем кнопку попробовать снова
+					slowHide(document.getElementById("refreshbtn"),"1.2s");
+				}, 800);
+		/*
 		document.getElementById("refreshbtn").addEventListener('transitionend', () => 
 			{
 			//показываем кнопку получить ответ
-			slowShow(document.getElementById("mainbtn"),slowShow);
+			slowShow(document.getElementById("mainbtn"),"4s");
 			//запуск мерцания div-кнопки 
 			divButtonInterval = setInterval(() => divFlicker(document.getElementById("mainbtn")), 2000); 
 			}, {once: true})
-		
+		*/
+		setTimeout(
+				function()
+					{
+						//показываем кнопку получить ответ
+						slowShow(document.getElementById("mainbtn"),"4s");
+						//запуск мерцания div-кнопки 
+						divButtonInterval = setInterval(() => divFlicker(document.getElementById("mainbtn")), 2000);
+					}, 6000);
+
+		clearInterval(divStringInterval);//остановка интервала мерцания div-текста
 	}
